0e81d432b64fe4e89ff3c9629922225cc2b49036
IsaacSemb <isaacsemb1996@gmail.com>
2025-04-09 20:01:52 +0100

    refactor: move file save manipulation functions for todos to a separate file
    Extracted file manipulation functions into `db_utils.py` for better modularity and maintainability.

---
+++ db_utils.py
@@ -0,0 +1,33 @@
+import os, json
+from typing import List
+from models import ToDo
+
+TODO_STORAGE_FILE = 'todo_db.json'
+
+
+# to write todos to file db
+def save_todos(todos: List[ToDo]) -> None:
+
+    with open(TODO_STORAGE_FILE, 'w') as f:
+
+        # convert all todos from pydantic format to normal dicts before dumping
+        data = [ todo.model_dump(mode='json') for todo in todos ]
+        json.dump(data, f, indent=2)
+
+
+# to read todos from db
+def load_todos() -> List[ToDo]:
+
+    # check if the storage exists
+    if not os.path.exists(TODO_STORAGE_FILE):
+        return []
+
+    with open(TODO_STORAGE_FILE, 'r') as f:
+
+        try:
+            data = json.load(f)
+            return [ ToDo(**item) for item in data ]
+
+        except json.JSONDecodeError:
+            print('Encountered error parsing storage')
+            return []
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 
 from fastapi import FastAPI
 from typing import List
-from models import ToDoCreate, ToDo
 from datetime import datetime
 
-import os
-import json
+
+from models import ToDoCreate, ToDo
+from db_utils import load_todos, save_todos
+
 
 
 # create app from fast api
@@ -21,33 +22,7 @@ todos: List[ToDo] = []
 
 # incooperating persistence into the app
 
-TODO_STORAGE_FILE = 'todo_db.json'
 
-
-def load_todos() -> List[ToDo]:
-    
-    # check if the storage exists
-    if not os.path.exists(TODO_STORAGE_FILE):
-        return []
-    
-    with open(TODO_STORAGE_FILE, 'r') as f:
-        
-        try:
-            data = json.load(f)
-            return [ ToDo(**item) for item in data ]
-        
-        except json.JSONDecodeError:
-            print('Encountered error parsing storage')
-            return []
-
-def save_todos(todos: List[ToDo]) -> None:
-    
-    with open(TODO_STORAGE_FILE, 'w') as f:
-        
-        # convert all todos from pydantic format to normal dicts before dumping
-        data = [ todo.model_dump(mode='json') for todo in todos ]
-        print(data)
-        json.dump(data, f, indent=2)
 
 # new list pulls from file
 todos: List[ToDo] = load_todos()
