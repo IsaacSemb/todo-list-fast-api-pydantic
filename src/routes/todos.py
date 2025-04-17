2ea88b2f5ef09574449f1c41f62fab20a5633907
IsaacSemb <isaacsemb1996@gmail.com>
2025-04-17 15:59:44 +0100

    Restructuriong all crud in an order that makes sense to me and adding necessary comments

--- src/routes/todos.py
+++ src/routes/todos.py
@@ -33,19 +33,38 @@ router = APIRouter()
 # new list pulls from file
 todos: List[ToDo] = load_todos()
 
-@router.get('/todos/{todo_id}', response_model=ToDo)
+# Create a new todo Item
+@router.post('/',response_model=ToDo)
+def create_todo( todo: ToDoCreate ):
+    
+    todos = load_todos()
+    
+    new_todo_item = ToDo(
+        id = len(todos)+1,
+        title = todo.title,
+        description = todo.description,
+        expected_completion = todo.expected_completion,
+        created_at = datetime.now(),
+        status = False
+    )
+    todos.append(new_todo_item)
+    save_todos(todos)
+    return new_todo_item
+
+
+# Retrieve all todo Items TODO: Incoperate pagination of some sort
+@router.get('/', response_model=List[ToDo])
+def get_all_todos():
+    return load_todos()
+
+# Retrieve single todo Item by id
+@router.get('/{todo_id}', response_model=ToDo)
 def get_todo(todo_id: int):
     return get_todo_by_id(todo_id)
 
-@router.delete('/todos/{todo_id}')
-def delete_todo(todo_id: int):
-    todos_list = load_todos()
-    target_todo_index = get_todo_index_by_todo_id(todo_id=todo_id, todos=todos_list)    
-    deleted_todo = todos_list.pop(target_todo_index)
-    save_todos(todos_list)
-    return {'message':"ToDo Deleted", 'details': deleted_todo }
-    
-@router.put('/todos/{todo_id}', response_model=None) 
+
+# Updating a Todo Item 
+@router.put('/{todo_id}', response_model=None) 
 def update_todo(todo_id: int, todo_update:ToDoUpdate):
     todos = load_todos()
     target_todo_index: ToDo = get_todo_index_by_todo_id(todo_id,todos)
@@ -74,25 +93,13 @@ def update_todo(todo_id: int, todo_update:ToDoUpdate):
         'old_todo':old_todo,
         'new_todo':target_todo
     }
-    
-    
-@router.post('/todos',response_model=ToDo)
-def create_todo( todo: ToDoCreate ):
-    
-    todos = load_todos()
-    
-    new_todo_item = ToDo(
-        id = len(todos)+1,
-        title = todo.title,
-        description = todo.description,
-        expected_completion = todo.expected_completion,
-        created_at = datetime.now(),
-        status = False
-    )
-    todos.append(new_todo_item)
-    save_todos(todos)
-    return new_todo_item
 
-@router.get('/todos', response_model=List[ToDo])
-def get_all_todos():
-    return load_todos()
+
+# Deleting a todo Item
+@router.delete('/{todo_id}')
+def delete_todo(todo_id: int):
+    todos_list = load_todos()
+    target_todo_index = get_todo_index_by_todo_id(todo_id=todo_id, todos=todos_list)    
+    deleted_todo = todos_list.pop(target_todo_index)
+    save_todos(todos_list)
+    return {'message':"ToDo Deleted", 'details': deleted_todo }
