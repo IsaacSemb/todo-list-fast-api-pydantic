4bc96d83bfe06e436cf5a5637bc9ffa760bbb393
IsaacSemb <isaacsemb1996@gmail.com>
2025-04-21 19:03:15 +0100

    Setup database crud file for todos
    - include stubs for all the necessary operations

---
+++ src/database_crud.py
@@ -0,0 +1,35 @@
+from sqlalchemy.orm import Session
+from src.schemas import todo_models, todo_schemas
+
+
+def create_todo():
+    """
+    To create a new to record and write it to db
+    """
+    pass
+
+
+def get_todo():
+    """
+    Retrieve a single to record by id
+    """
+    pass
+
+
+def get_todos():
+    """
+    Retrieve a list of todo records paginated by 10 items per page
+    """
+    pass
+
+
+def update_todo():
+    """
+    Update part or fully a todo record 
+    """
+    pass
+
+
+def delete_todo():
+    """Delete a todo record"""
+    pass
