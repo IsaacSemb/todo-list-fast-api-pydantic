f94115a5d6ad8f80ba5380e10269416bbaa3ccb7
IsaacSemb <isaacsemb1996@gmail.com>
2025-07-01 15:24:04 +0100

    add more utility functions

--- utils/seed_database.py
+++ utils/seed_database.py
@@ -16,7 +16,7 @@ print(root_dir)
 if root_dir not in sys.path:
     sys.path.append(root_dir)
 
-from app import database, crud, schemas
+from app import database, crud, models, schemas
 
 def get_json_data(file):
     
@@ -31,7 +31,6 @@ data = get_json_data('todo_fake_data.json')
 
 db = database.SessionLocal()
 
-print(db)
 
 notes = """
 
@@ -66,5 +65,31 @@ def testing_database_reset():
     crud.create_todo(db, sample_todo)
     
 
-testing_database_reset()
+# testing_database_reset()
 
+
+def insert_many_todos(data = None):
+    data = [
+        {
+            "title": "Learn FastAPI",
+            "description": "Keep pushing!",
+            "created_at": "2025-04-09T20:00:49.528982",
+            "expected_completion": "2025-04-12T10:00:00",
+            "status": False
+        },
+        {
+            "title": "Wake Up",
+            "description": "You need to read your books!",
+            "created_at": "2025-04-09T20:01:28.009811",
+            "expected_completion": "2025-04-12T10:00:00",
+            "status": True
+        }
+        ]
+    todos = [schemas.ToDoCreate(**item) for item in data]
+    crud.create_many_todos(db, todos)
+
+insert_many_todos()
+
+
+def reset_a_table(table):
+    return crud.reset_table(database.db_engine, table)
