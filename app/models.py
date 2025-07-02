6e167888fa2c05b3302a1f9f1df1056cfa246be2
IsaacSemb <isaacsemb1996@gmail.com>
2025-07-02 18:56:31 +0100

    Add the user model for users in the system

--- app/models.py
+++ app/models.py
@@ -24,3 +24,16 @@ class Todo(Base):
     created_at = sa.Column(sa.DateTime(timezone=True), default=get_current_utc, nullable=False)
     updated_at = sa.Column(sa.DateTime(timezone=True), default=get_current_utc, onupdate=get_current_utc, nullable=False)
 
+class User(Base):
+    """
+    SQLAlchemy ORM model for the users table.
+    This is the database facing model for users of the system.
+    """
+    
+    __tablename__ = "users"
+
+    id = sa.Column(sa.Integer, primary_key=True, index=True)
+    username = sa.Column(sa.String, unique=True, index=True, nullable=False)
+    email = sa.Column(sa.String, unique=True, index=True, nullable=False)
+    hashed_password = sa.Column(sa.String, nullable=False)
+    
