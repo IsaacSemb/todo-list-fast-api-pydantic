19a1ae465aec8ae616c3255bce8f8ac6cbba0888
IsaacSemb <isaacsemb1996@gmail.com>
2025-07-02 19:18:59 +0100

    create relationship between todos and users

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 
 from datetime import datetime, timezone
 import sqlalchemy as sa
+import sqlalchemy.orm as so
 from app.database import Base
 
 def get_current_utc():
@@ -23,6 +24,15 @@ class Todo(Base):
     status = sa.Column(sa.Boolean, default=False)
     created_at = sa.Column(sa.DateTime(timezone=True), default=get_current_utc, nullable=False)
     updated_at = sa.Column(sa.DateTime(timezone=True), default=get_current_utc, onupdate=get_current_utc, nullable=False)
+    
+    
+    # foreign key to users table
+    #  CASCADE mean if user is deleted, delete the todos too
+    user_id = sa.Column(sa.Integer, sa.ForeignKey(column='users.id', ondelete="CASCADE"))
+    
+    # internal ORM relation between user and todos
+    owner = so.relationship('User', back_populates='todos')
+
 
 class User(Base):
     """
@@ -37,3 +47,6 @@ class User(Base):
     email = sa.Column(sa.String, unique=True, index=True, nullable=False)
     hashed_password = sa.Column(sa.String, nullable=False)
     
+    
+    # internal ORM relation between todos and user
+    todos = so.relationship('Todo', back_populates='owner')
