e5aff86c80deeffd64bf11b44f6ce8d553598128
IsaacSemb <isaacsemb1996@gmail.com>
2025-07-08 23:07:29 +0100

    Add JWT flow to the the security module to streamline creation of an access token

--- app/core/security.py
+++ app/core/security.py
@@ -1,6 +1,14 @@
 
+from datetime import datetime, timedelta, timezone
 import secrets
 from passlib.context import CryptContext
+from jose import jwt, JWTError
+
+
+# Generate a secure random secret key (uncomment to use in production)
+SECRET_KEY = "kachabali-XTRA" or secrets.token_urlsafe(32)
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 # pwd_context is a Passlib CryptContext instance used to hash and verify passwords securely.
@@ -27,3 +35,38 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
         bool: True if the plain password matches the hashed password, False otherwise.
     """
     return pwd_context.verify(plain_password, hashed_password)
+
+
+#  ================= JWT STUFF ======================
+
+# STANDARD JWT STUFF 
+# 
+# "iss" → issuer
+# "sub" → subject (who it's about)
+# "aud" → audience
+# "exp" → expiration time
+# "nbf" → not before
+# "iat" → issued at
+
+def create_access_token(data: dict, expires_in: timedelta | None = None):
+    """
+    this is a helper function to create a JWT token to use in auth flow
+    """
+    # make a copy of the data to encode in our case we have started with email
+    data_to_encode = data.copy()
+    
+    # This is the full UTC aware period for the expiry of this token
+    expiry_datetime = datetime.now(timezone.utc) + (expires_in or timedelta(minutes=30))
+    
+    # get the data to encode and add the expiry part to it 
+    data_to_encode.update({"exp": expiry_datetime})
+    
+    # return the entire token with all the good stuff 
+    # like data and expiry 
+    return jwt.encode(data_to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_access_token(token: str):
+    try:
+        return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except JWTError:
+        return None
